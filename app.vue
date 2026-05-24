66f3cc2211c757793ffc76a6827a8424b5c6efb8
tzuling2001 <ling0967253511@gmail.com>
2026-05-24 21:58:33 +0800

    feat: 套件建置

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="min-h-screen bg-slate-50 text-slate-900">
-    <NuxtRouteAnnouncer />
+  <div class="min-h-screen">
+    <!-- <NuxtRouteAnnouncer /> -->
     <NuxtPage />
   </div>
 </template>
